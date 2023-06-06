f441f32bd6e49b0a41a68e0efefd13634f2d352c
NicolasMazzey <nicolasmazzey@gmail.com>
2023-06-06 19:46:51 -0300

    commit

---
+++ SimuladorAscensores/src/main/java/com/mycompany/simuladorascensores/SimuladorAscensores.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.simuladorascensores;
+
+
+public class SimuladorAscensores {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
